c5090aaf8969987c2ec69a6c7e7bfe4d83d7cc7e
saoufi sami <saoufi-sami@hotmail.fr>
2024-11-20 22:49:34 +0100

    premier  css ok

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
     <script defer src="public/js/index.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <title>Todo </title>
 </head>
+
+
 <body>
-  <header>HEADER</header>
-<div class="content">CONTENT</div>
-<footer>FOOTER</footer>
+    <div class="container">
+        <header>
+        <div class="logo">Php todo</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+            <h1>Ma Todo</h1>
+            <div class="todo-form"></div>
+            <div class="todo-list"></div>
+        </div>
+        </div>
+        <footer>
+            <p>2024 Todo Php. Tous droits réservés.</p>
+        </footer>
 
-
-
+    </div>
 
 </body>
+
 </html>
